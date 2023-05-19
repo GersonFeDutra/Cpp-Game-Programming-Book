b156b256aa99be2bad813ef6f7486c768272b6ff
GersonFeDutra <gerson.creative.des@gmail.com>
2023-05-18 22:58:04 -0300

    The main game loop

--- Ch.01_TimTheTimber/main.cpp
+++ Ch.01_TimTheTimber/main.cpp
@@ -4,6 +4,9 @@
 // Make code easier to type with "using namespace"
 using namespace sf;
 
+// Resources paths
+#define BACKGROUND_PNG "assets/graphics/background.png"
+
 // This is where our game starts from
 int main()
 {
@@ -12,6 +15,67 @@ int main()
 
     // Create and open a window for this game
     RenderWindow window(va, "TimTheTimber", Style::Fullscreen);
+    
+    /*
+     ************************************************************
+     * Load persistent resources
+     ************************************************************
+     */
+    
+    // Create a texture to hold a graphic on the GPU
+    Texture textureBackground;
+    
+    // Load a graphic into the texture
+    textureBackground.loadFromFile(BACKGROUND_PNG);
+    
+    // Create a sprite
+    Sprite spriteBackground;
+    
+    // Attach the texture to the sprite
+    spriteBackground.setTexture(textureBackground);
+    
+    // Set the spriteBackground to cover teh screen
+    spriteBackground.setPosition(0, 0);
+
+    /*
+     ************************************************************
+     * Main Loop
+     ************************************************************
+     */
+
+    while (window.isOpen()) {
+        /*
+         ************************************************************
+         * Handle the players input
+         ************************************************************
+         */
+
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+            window.close();
+        
+        
+        /*
+         ************************************************************
+         * Update the scene
+         ************************************************************
+         */
+        
+        /*
+         ************************************************************
+         * Draw the scene
+         ************************************************************
+         */
+
+        // Clear everything from the last frame
+        window.clear();
+
+        // Draw our game scene here
+        window.draw(spriteBackground);
+        
+        // Show everything we just draw
+        window.display();
+
+    }
 
     return 0;
 }
